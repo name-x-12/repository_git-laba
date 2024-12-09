527fbd195260fe2dc1a923704dbfddaf454c2dc5
Anton <anton.tarakanov182@gmail.com>
2024-12-09 09:58:19 +0500

    Add files via upload

---
+++ git_lab.cpp
@@ -0,0 +1,24 @@
+#include "git_lab.h";
+
+int main(){
+    srand(static_cast<unsigned int>(time(0)));
+    cout<<"Сыграем в игру 'Угадай число(быки и коровы)'\n";
+    cout<<("Выберите диапозон случайного числа: \n");
+    int start,end;
+    cout<<"start: ";
+    cin>>start;
+    
+    cout<<"end: ";
+    cin>>end;
+
+    int R_Number=RandomNumber(start,end);
+    int userNum=0;
+    cout<<"Я загадал число от "<<start<<" до "<<end<<". Угадай его."<<endl;
+
+    while(userNum!=R_Number){
+        userNum=GetUserInput();
+        Check(R_Number,userNum);
+    }    
+
+    return 0;
+}
---
+++ git_lab.h
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+#include <cstdlib>
+#include <ctime>
+#include <sstream>
+#include <string> 
+
+
+
+
+
+//рандомное число
+int RandomNumber(int start,int end){
+    int number=rand()%(end-start+1)+start;
+    return number;
+}
+
+
+//считывание ответа
+int GetUserInput(){
+    int num;
+    cout<<"Введите ваше предложение: ";
+    cin>>num;
+    return num;
+}
+
+
+//проверка ответа
+void Check(int RandomNumber,int UserNum){
+    if(UserNum<RandomNumber) cout<<"Загаданное число больше. "<<endl;
+    else if(UserNum>RandomNumber) cout<<"Загаданное число меньше. "<<endl;
+    else cout<<"Вы угадали!"<<endl;
+}
+
+//d
